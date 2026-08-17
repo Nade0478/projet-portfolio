526830d4e08d61b4f71a3fd4fdf1c20f69c36e65
Nade04 <86410129+Nade0478@users.noreply.github.com>
2026-08-17 05:48:35 +0200

    modif carousel test

--- src/pages/portfolio/CarouselTest.jsx
+++ src/pages/portfolio/CarouselTest.jsx
@@ -1,20 +1,25 @@
+import React from "react";
 import CarouselCaptureTest from "../../component/CarouselCaptureTest";
 
-export default function CarouselTest() {
-  const captures = [
-    {
-      name: "Test Lighthouse SEO",
-      file: "/assets/capture-test-lighthouse-seo.png",
-    },
-    {
-      name: "Test Postman API",
-      file: "/assets/capture-test-postman.png",
-    },
-    {
-      name: "Tests divers",
-      file: "/assets/Capture-tests.png",
-    },
-  ];
+function importAll(r) {
+  return r.keys().map((key) => ({
+    name: key.replace("./", ""),
+    file: r(key),
+  }));
+}
 
-  return <CarouselCaptureTest captures={captures} />;
+const captures = importAll(
+  require.context("../../assets/capture-test", false, /\.(png|jpe?g|svg)$/)
+);
+
+export default function CaptureTest() {
+  return (
+    <div className="page-container">
+      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>
+        Captures de Tests
+      </h1>
+
+      <CarouselCaptureTest captures={captures} />
+    </div>
+  );
 }
