108aa30a150e9c493cf0f218afc63585a24e7917
Nade04 <86410129+Nade0478@users.noreply.github.com>
2026-08-23 19:46:20 +0200

    commit de mise à jour

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,7 +7,7 @@ export default function home() {
   return (
     <div className="portfolio-container">
       <img
-        src="public\photoPourLinkdin.jpg"
+        src="/photoPourLinkdin.jpg"
         alt="Nadège Gautier"
         className="portfolio-photo"
       />
@@ -16,37 +16,37 @@ export default function home() {
       <Presentation />
 
       <div className="portfolio-grid">
-        <Link to="/dev" className="portfolio-card1">
+        <Link to="/metier/dev" className="portfolio-card1">
           <h3>Développement Web</h3>
           <p>React, Laravel, API, Front et Back</p>
         </Link>
 
-        <Link to="/test" className="portfolio-card3">
+        <Link to="/metier/test" className="portfolio-card3">
           <h3>Testeur ISTQB</h3>
           <p>Tests fonctionnels et automatisations</p>
         </Link>
 
-        <Link to="/projet" className="portfolio-card4">
+        <Link to="/metier/projet" className="portfolio-card4">
           <h3>Gestion de Projet</h3>
           <p>Organisation, planification, pilotage</p>
         </Link>
 
-        <Link to="/cyber" className="portfolio-card5">
+        <Link to="/metier/cyber" className="portfolio-card5">
           <h3>Cybersécurité</h3>
           <p>Analyse, prévention, bonnes pratiques</p>
         </Link>
 
-        <Link to="/adm" className="portfolio-card6">
+        <Link to="/metier/adm" className="portfolio-card6">
           <h3>Assistante Administrative</h3>
           <p>Pack Office, gestion d'agenda, traitement de dossiers</p>
         </Link>
 
-        <Link to="/cons" className="portfolio-card7">
+        <Link to="/metier/cons" className="portfolio-card7">
           <h3>Conseillère clientèle à distance</h3>
           <p>Gestion des demandes, prise de rendez-vous, SAV</p>
         </Link>
 
-        <Link to="/div" className="portfolio-card2">
+        <Link to="/metier/div" className="portfolio-card2">
           <h3>Divers</h3>
           <p>Anglais, autres compétences</p>
         </Link>
